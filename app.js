3f5577fcbf7e90191c756461153efa7d43148411
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-21 00:49:42 -0800

    Initial quick & dirty version (very quick & very dirty)

---
+++ app.js
@@ -0,0 +1,83 @@
+var http = require('http');
+var global = { events: require('events'),
+               require: require,
+               console: console,
+               sys: require('sys'),
+               messaging: require('messaging'),
+               dist: require('dist')
+             };
+
+require.paths.unshift(__dirname);
+require('showdown');
+var _showdown = new Showdown.converter();
+var handler = function(request, response) {	 
+	if (request.path == "/favicon.ico") {
+		response.writeHead(404,{});
+		return;
+	} 
+	
+	var tokens = request.path.split("/");
+    tokens.shift();
+
+	var moduleName = tokens.shift();
+	if (moduleName == "") {
+		moduleName = "";
+		module = global;
+	} else {
+		var module = global[moduleName];
+		if (module == undefined) {
+		 	response.writeHead(200,{"Content-Type": "text/html"});
+		 	response.end("<h1>Module not found</h1>");
+		    return;
+		}
+    }
+
+    var current = module;
+    var key = moduleName;
+	var lkey = moduleName;
+    while (tokens.length > 0) {
+	  lkey = tokens.shift();
+	  key += "." + lkey;
+	  if ((current = current[lkey]) == undefined) {
+		 response.writeHead(200,{"Content-Type": "text/html"});
+		 response.end("<h1>Export not found</h1>");
+		 return;
+ 	  }
+    }
+
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("<html><head><title>Beam.js Doc Browser</title></head><body>");
+
+	var __doc__ = "";
+	
+	if ((d = current['__doc__']) != undefined) {
+		   __doc__ = _showdown.makeHtml(d);
+	}
+
+	
+	response.write("<h1><em>" + typeof current + "</em> " + key + "</h1>");
+	
+	response.write(__doc__);
+	
+	for (_export in current) {
+		if ((_export != "__doc__") && ((typeof current[_export] == 'object') || (typeof current[_export] == 'function')))
+		 {
+			vkey = lkey;
+			if (lkey != "") {
+				vkey = "/" + lkey;
+			}
+			response.write("<h2><em>" + typeof current[_export] + '</em> <a href="' + lkey + '/' + _export +'">' + _export + "</a></h2>");
+			__doc__ = ""
+			if ((d = current[_export]['__doc__']) != undefined) {
+			   	__doc__ = _showdown.makeHtml(d);
+			}
+			response.write(__doc__);
+		}
+	}
+	
+	response.write("<hr><em>Beam.js " + global.sys.beamjs.version + "</em>");
+	
+	response.end("</body></html>");
+	
+};
+http.createServer(handler).listen(listenPort);
