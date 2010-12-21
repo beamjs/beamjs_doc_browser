600e8e95048d7e98c3cf6fbd42c7f3dce106e99e
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-21 10:50:25 -0800

    Moved to a much saner template-based rendering (YAJET with very few minor modifications)

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ var global = { events: require('events'),
 
 require.paths.unshift(__dirname);
 require('showdown');
-var _showdown = new Showdown.converter();
+
+var YAJET = require('yajet');
+
+var Template = function (v) { return v};
+
 var handler = function(request, response) {	 
 	if (request.path == "/favicon.ico") {
 		response.writeHead(404,{});
@@ -17,70 +21,57 @@ var handler = function(request, response) {
 	} 
 	
 	var tokens = request.path.split("/");
-    tokens.shift();
+    if (request.path=="/") tokens.shift(); // get rid of empty string before the slash
 
-	var moduleName = tokens.shift();
-	if (moduleName == "") {
-		moduleName = "";
-		module = global;
-	} else {
-		var module = global[moduleName];
-		if (module == undefined) {
-		 	response.writeHead(200,{"Content-Type": "text/html"});
-		 	response.end("<h1>Module not found</h1>");
-		    return;
-		}
-    }
-
-    var current = module;
-    var key = moduleName;
-	var lkey = moduleName;
+    var current = {"": global};
+    var key="", lkey; 
+	var path = "";
+	
     while (tokens.length > 0) {
 	  lkey = tokens.shift();
-	  key += "." + lkey;
+	  if (lkey == "") {
+	    key += "(global)";
+	  } else {
+	  	key += "." + lkey;
+	  }
+	  path += lkey + "/" ;
 	  if ((current = current[lkey]) == undefined) {
 		 response.writeHead(200,{"Content-Type": "text/html"});
-		 response.end("<h1>Export not found</h1>");
+		 response.end("<h1>Not found</h1>");
 		 return;
  	  }
     }
 
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("<html><head><title>Beam.js Documentation Browser</title></head><body>");
+    var templateData = {
+	    doc: "",
+	    key: key,
+	    topics: {},
+	    path: path,
+	    version: global.sys.beamjs.version
+    };
 
-	var __doc__ = "";
-	
 	if ((d = current['__doc__']) != undefined) {
-		   __doc__ = _showdown.makeHtml(d);
+		   templateData.doc= d;
 	}
-
-	
-	if (key!="") {
-		response.write("<h1><em>" + typeof current + "</em> " + key + "</h1>");
-	}
-	
-	response.write(__doc__);
 	
 	for (_export in current) {
 		if ((_export != "__doc__") && ((typeof current[_export] == 'object') || (typeof current[_export] == 'function')))
 		 {
-			vkey = lkey;
-			if (lkey != "") {
-				vkey = "/" + lkey;
-			}
-			response.write("<h2><em>" + typeof current[_export] + '</em> <a href="' + lkey + '/' + _export +'">' + _export + "</a></h2>");
-			__doc__ = ""
-			if ((d = current[_export]['__doc__']) != undefined) {
-			   	__doc__ = _showdown.makeHtml(d);
-			}
-			response.write(__doc__);
-			response.write('<hr />');
+			templateData.topics[_export] = current[_export];
 		}
 	}
 	
-	response.write('Powered by <a href="https://github.com/beamjs/beamjs_doc_browser">Beam.js Documentation Browser</a> (<em>Beam.js '+ global.sys.beamjs.version + "</em>)");
-	
-	response.end("</body></html>");
-	
+	response.writeHead(200, {"Content-Type": "text/html"});
+	try {
+		response.end(Template(templateData));
+	} catch (e) {
+		console.log(e);
+	}
 };
-http.createServer(handler).listen(listenPort);
+var showdown = new Showdown.converter();
+require('fs').readFile(__dirname + "/template.html", function(err, template) {
+	Template = new YAJET({reader_char: "$",	filters : {
+        showdown: function(val) { return showdown.makeHtml(val) }
+    }}).compile(template);
+ 	http.createServer(handler).listen(listenPort);
+});
