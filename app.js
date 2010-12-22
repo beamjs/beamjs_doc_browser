732d6deccf126308a02acf2380652e7f68e068b0
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-22 10:22:46 -0800

    Don't show functions' isNative property, it is a system property and I need a better way to conceal it (proper fix)

--- app.js
+++ app.js
@@ -67,7 +67,9 @@ var handler = function(request, response) {
     }
 
     for (_export in current) {
-        if ((_export != "__doc__") && ((typeof current == 'function')  && (_export != "isNative")) && ((typeof current[_export] == 'object') || (typeof current[_export] == 'function'))) {
+        if ((_export != "__doc__") && ((typeof current[_export] == 'object') || (typeof current[_export] == 'function'))) {
+	        if ((typeof current == 'function') && (_export == "isNative"))
+	           continue;
             templateData.topics[_export] = current[_export];
         }
     }
