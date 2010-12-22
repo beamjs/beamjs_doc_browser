d9271b08a5e2cc44e2aca339b7c7ff3a116ef58b
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-22 10:17:18 -0800

    Don't print 'object' type for modules/objects, it is kind of confusing anyway

--- app.js
+++ app.js
@@ -88,7 +88,8 @@ require('fs').readFile(__dirname + file, function(err, data) {
 
 require('fs').readFile(__dirname + "/template.html", function(err, template) {
 	Template = new YAJET({reader_char: "$",	filters : {
-        showdown: function(val) { return showdown.makeHtml(val) }
+        showdown: function(val) { return showdown.makeHtml(val) },
+        typeOf: function(val) { if (typeof val == 'function') return 'function'; if (typeof val == 'object') return ''; }
     }}).compile(template);
  	http.createServer(handler).listen(listenPort);
 });
